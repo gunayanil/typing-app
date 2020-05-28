1e529fc8ee78b0bc393192393fc479307e5e0b73
gunayanil <gunayanilemre@gmail.com>
2020-05-28 14:27:34 -0400

    imported styled components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import CTA from '../styled/CTA';
+import { StyledTitle } from '../styled/Misc';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <StyledTitle>Ready to type?</StyledTitle>
+      <CTA to='/game'>Click or type 's' to start!</CTA>
+    </div>
+  );
 };
 
 export default Home;
