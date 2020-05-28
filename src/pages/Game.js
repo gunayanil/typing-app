d9ef7ba1ef82f668f9b9d84d11136d35545e3db1
gunayanil <gunayanilemre@gmail.com>
2020-05-28 14:43:04 -0400

    imported sytyled components and implemented main structure

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {
+  StyledGame,
+  StyledScore,
+  StyledTimer,
+  StyledCharacter,
+} from '../styled/Game';
+import { Strong } from '../styled/Misc';
 
 const Game = () => {
-  return <div></div>;
+  return (
+    <StyledGame>
+      <StyledScore>
+        Score: <Strong>0</Strong>
+      </StyledScore>
+      <StyledCharacter>A</StyledCharacter>
+      <StyledTimer>
+        Timer: <Strong>00:00</Strong>
+      </StyledTimer>
+    </StyledGame>
+  );
 };
 
 export default Game;
