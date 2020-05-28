9ce10ed382ba0b4a83275a81a20c5c0297177436
gunayanil <gunayanilemre@gmail.com>
2020-05-28 09:19:57 -0400

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import HighScores from './pages/HighScores';
+import GameOver from './pages/GameOver';
+import Navbar from './components/Navbar';
+import { Container } from './styled/Container';
+import { Main } from './styled/Main';
+import Global from './styled/Global';
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Global />
+      <Main>
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route path='/game' component={Game} />
+            <Route path='/highscores' component={HighScores} />
+            <Route path='/gameover' component={GameOver} />
+            <Route path='/' component={Home} />
+          </Switch>
+        </Container>
+      </Main>
+    </Router>
+  );
 }
 
 export default App;
