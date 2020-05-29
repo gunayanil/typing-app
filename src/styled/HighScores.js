badf8397159eb27e92ddf53e78b6eb38a25e9f30
gunayanil <gunayanilemre@gmail.com>
2020-05-29 18:50:31 -0400

    simple styles for highscores page

---
+++ src/styled/HighScores.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ScoresList = styled.ol`
+  /*style later */
+`;
+
+export const ScoreLI = styled.li`
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
+`;
