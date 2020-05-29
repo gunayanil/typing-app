e12c027a60f5ba83864682af80cc7bd0b490fabf
gunayanil <gunayanilemre@gmail.com>
2020-05-29 18:51:25 -0400

    listed high scores

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ScoresList, ScoreLI } from '../styled/HighScores';
 
 const HighScores = () => {
-  return <div></div>;
+  const [highScores, setHighScores] = useState([]);
+
+  // callback func itself shouldn't be async, however, we can call async func inside it
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch('/.netlify/functions/getHighScores');
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    loadHighScores();
+  }, []);
+
+  return (
+    <div>
+      <h1>High Scoress</h1>
+      <ScoresList>
+        {highScores.map(score => (
+          <ScoreLI key={score.id}>
+            {score.fields.name} - {score.fields.score}
+          </ScoreLI>
+        ))}
+      </ScoresList>
+    </div>
+  );
 };
 
 export default HighScores;
