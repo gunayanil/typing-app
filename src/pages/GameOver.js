bdde7d92ec12468761716c39ee1fe775b0248c03
gunayanil <gunayanilemre@gmail.com>
2020-05-29 22:47:28 -0400

    added game over logic

--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useScore } from '../context/ScoreContext';
 import { StyledLink } from '../styled/Navbar';
+import { StyledCharacter } from '../styled/Game';
 
 const GameOver = ({ history }) => {
   const [score] = useScore();
+  const [message, setMessage] = useState('');
+
   if (score === -1) {
     history.push('/');
   }
+
+  useEffect(() => {
+    const saveHighScore = async () => {
+      try {
+        const options = {
+          method: 'POST',
+          body: JSON.stringify({ name: 'Emre', score }),
+        };
+        const res = await fetch('/.netlify/functions/saveHighScore', options);
+        const data = await res.json();
+
+        if (data.id) {
+          setMessage('Congrats! You got a high score.');
+        } else {
+          setMessage('Not enough. Keep trying!');
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    saveHighScore();
+  }, [score]);
+
   return (
     <div>
       <h1>Game Over!</h1>
-      <p>{score}</p>
+      <StyledCharacter>{score}</StyledCharacter>
+      <p>{message}</p>
       <StyledLink to='/'>Go Home</StyledLink>
       <StyledLink to='/game'>Play Again</StyledLink>
     </div>
