c2565aea5757e3437d01d034931246a08998b902
gunayanil <gunayanilemre@gmail.com>
2020-05-30 17:22:48 -0400

    added validateAccessToken functionality

--- functions/utils/auth.js
+++ functions/utils/auth.js
@@ -1,3 +1,12 @@
+const jwt = require('jsonwebtoken');
+const jwks = require('jwks-rsa');
+const { promisify } = require('util');
+
+const jwksClient = jwks({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  audience: process.env.AUTH0_AUDIENCE,
+});
+
 const getAccessToken = headers => {
   const rawAuthorization = headers.authorization;
   if (!rawAuthorization) {
@@ -13,6 +22,24 @@ const getAccessToken = headers => {
   return accessToken;
 };
 
+const validateAccessToken = async token => {
+  try {
+    const decodedToken = jwt.decode(token, { complete: true });
+    const kid = decodedToken.header.kid;
+    const getSigningKey = promisify(jwksClient.getSigningKey);
+    const key = await getSigningKey(kid);
+    const signingKey = key.publicKey;
+    const options = { algorithms: 'RS256' };
+
+    jwt.verify(token, signingKey, options);
+    return decodedToken.payload;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+};
+
 module.exports = {
   getAccessToken,
+  validateAccessToken,
 };
